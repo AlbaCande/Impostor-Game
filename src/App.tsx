7cde16c2d76875bb287d02c5e43ba947a339d6a8
Alba Candelario <alba.candelario-matas@epitech.eu>
2024-05-20 19:05:13 +0200

    [add] Shadcn as dependency and set it up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import type { FC } from 'react';
 import './App.css';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 
 const App: FC = () => {
 	return (
-		<h1 className="text-3xl font-bold underline">
-			Hello world!
-		</h1>
+		<>
+			<h1 className="text-3xl font-bold underline">
+				Hello world!
+			</h1>
+			<Avatar>
+				<AvatarFallback>AC</AvatarFallback>
+			</Avatar>
+		</>
 	);
 };
 
